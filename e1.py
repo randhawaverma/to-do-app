e5afb0e7fc48829e3cc7dfc31e4a18e22ac41864
randhawa-verma <randhawaverma@gmail.com>
2023-09-03 08:52:27 +0530

    Webbrowser util

--- e1.py
+++ e1.py
@@ -1,6 +1,8 @@
 import glob
 import shutil
+import webbrowser
 
+# 1
 myfiles = glob.glob("files/*.txt")
 print(myfiles)
 
@@ -8,4 +10,9 @@ for filepath in myfiles:
     with open(filepath) as file:
         print(file.read())
 
+# 2
 shutil.make_archive("output", "zip", "files")
+
+# 3
+search = input("Enter search term: ").replace(" ", "+")
+webbrowser.open("https://google.com/search?q=" + search)
